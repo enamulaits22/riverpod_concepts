7e151dfd8502eccb8d9ebfed217e56c467734e8b
enamulaits22 <enamulaits22@gmail.com>
2022-05-30 16:28:26 +0600

    completed newsapp with riverpod

--- state_notifier/lib/presentatioin/Home/home.dart
+++ state_notifier/lib/presentatioin/Home/home.dart
@@ -3,49 +3,43 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:test_app/data/notifier/news_notifier.dart';
 import 'package:test_app/data/notifier/news_state.dart';
 
-class Home extends StatelessWidget {
+class Home extends ConsumerWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, ref) {
     // ref.read(newsDataProvider.notifier).getNews();
-    // NewsState state = ref.watch(newsDataProvider);
+    NewsState state = ref.watch(newsDataProvider);
     return Scaffold(
-      // appBar: AppBar(title: const Text('News data')),
-      // floatingActionButton: FloatingActionButton(onPressed: (){
-      //   ref.read(newsDataProvider.notifier).getNews();
-      // }),
-      body: Consumer(
-        builder: (context, ref, child) {
-          NewsState state = ref.watch(newsDataProvider);
-          if (state is NewsLoadingState) {
-            return const Center(child: CircularProgressIndicator());
-          }
-          if (state is NewsLoadedState) {
-            return Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Expanded(
-                      child: ListView.builder(
-                        shrinkWrap: true,
-                        itemCount: state.news.length,
-                        itemBuilder: (context, index) {
-                          return Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(state.news[index].description!),
-                              const Divider(),
-                            ],
-                          );
-                        },
-                      ),
-                    ),
-                  ],
-                );
-          }
-          return Container();
-        },
-      ),
+      appBar: AppBar(title: const Text('News data')),
+      floatingActionButton: FloatingActionButton(onPressed: (){
+        ref.read(newsDataProvider.notifier).getNews();
+      }),
+      body: state is NewsLoadingState
+        ? const Center(child: CircularProgressIndicator())
+        : state is NewsLoadedState ? Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Expanded(
+                  child: ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: state.news.length,
+                    itemBuilder: (context, index) {
+                      return Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(state.news[index].description!),
+                          const Divider(),
+                        ],
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          )
+        : Container(),
     );
   }
 }
